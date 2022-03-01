3eaa474b071dcbf95adf5b325bd36a71879e6107
fedotarte <artyom.fedotov93@gmail.com>
2022-03-01 21:05:25 +0300

    added header with types for modules scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useMemo, useEffect } from 'react';
 import { BrowserRouter as Router, Link, useLocation } from 'react-router-dom';
 import Form from './Form';
+import Header from './Header';
 
 function App() {
     return (
         <Router>
+            <Header />
             <Form />
         </Router>
     );
